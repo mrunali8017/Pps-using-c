6c0caa0416d1dfdffee2e60f0221186dea6ed47e
mrunali8017 <panchalmrunali3@gmail.com>
2025-05-01 11:54:40 +0530

    Create Avg No.c

---
+++ Avg No.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int num1, num2;
+    float avg;
+    printf("Enter first number");
+    scanf("%d", &num1);
+    printf("Enter second number");
+    scanf("%d", &num2);
+    avg = (num1 + num2) / 2;
+    printf("The average of %d and %d is %.2f\n", num1, num2, avg);
+    return 0;
+}
