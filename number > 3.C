9cb464fa8d9d4a03b3bcb0b5ef2ec24749626e0e
mrunali8017 <panchalmrunali3@gmail.com>
2025-05-02 20:22:13 +0530

    Create number > 3.C

---
+++ number > 3.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter the number");
+    scanf("%d", &number);
+    if (number > 3)
+    {
+        printf("yes");
+    }
+    printf("End of Program");
+    return 0;
+}
