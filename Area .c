2c1a66778d6272cf3f2cf602b36903f9802b9474
mrunali8017 <panchalmrunali3@gmail.com>
2025-06-01 17:22:11 +0530

    Update Area .c

--- Area .c
+++ Area .c
@@ -2,11 +2,16 @@
 int main()
 {
     int length, breadth, area, perimeter;
-    printf("Enter the length of the rectangle: ")    scanf("%d", &length);
+    
+    printf("Enter the length of the rectangle");     
+    scanf("%d", &length);
+    
     printf("Enter the breadth of the rectangle");
     scanf("%d", &breadth);
+    
     area = length * breadth;
     perimeter = 2 * (length + breadth);
+    
     printf("Area of the rectangle %d\n", area);
     printf("Perimeter of the rectangle %d\n", perimeter);
 
