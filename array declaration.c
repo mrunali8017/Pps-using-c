f9d88f22108a8b491aaee6f364da6f56d3617e89
mrunali8017 <panchalmrunali3@gmail.com>
2025-06-17 19:30:25 +0530

    Create array declaration.c

---
+++ array declaration.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a[4];
+
+    a[0] = 35;
+    a[1] = 45;
+    a[2] = 42;
+    a[3] = 49;
+
+    printf("%d\n", a[2]);
+    printf("%d\n", a[3]);
+
+    return 0;
+}
