f139ca3932e397b8496c29a5f3a49c1bd7ebe356
mrunali8017 <panchalmrunali3@gmail.com>
2025-06-17 19:37:55 +0530

    Create 2d Array.c

---
+++ 2d Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, a[3][4];
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
